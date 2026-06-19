d46e762ff2da325e4fc87232397182eec8d150c1
sya-ri <git@s7a.dev>
2026-06-19 16:18:42 +0900

    chore: remove tmux auto start

--- zsh/.config/dotfiles/zsh-init.zsh
+++ zsh/.config/dotfiles/zsh-init.zsh
@@ -1,9 +1,5 @@
 # dotfiles zsh initialization.
 
-if command -v tmux >/dev/null 2>&1 && [ -z "$TMUX" ] && [ -n "$PS1" ]; then
-  tmux attach-session -t main || tmux new-session -s main
-fi
-
 eval "$(starship init zsh)"
 
 export DOCKER_HOST="unix://${HOME}/.colima/default/docker.sock"
